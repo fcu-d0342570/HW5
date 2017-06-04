3942335c0f3d0299900b5f9a044daf6d9a50b521
Ding <i1239a8e@gmail.com>
2017-06-04 09:40:16 +0800

    test

---
+++ app/src/main/java/com/example/i1239/hw5/AlbumItem.java
@@ -0,0 +1,16 @@
+package com.example.i1239.hw5;
+
+/**
+ * Created by i1239 on 2017/6/4.
+ */
+public class AlbumItem {
+    AlbumItem(String name, int imgid, int score) {
+        this.name = name;
+        this.imgid = imgid;
+        this.score = score;
+    }
+
+    String name;
+    int imgid;
+    int score;
+}
